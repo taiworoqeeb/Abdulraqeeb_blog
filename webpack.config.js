d51427075880f0c06e0c273754984468a2a768b9
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-09-05 20:04:53 +0100

    -a

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+ 
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.s|css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" })
+  ],
+  optimization: {
+    nodeEnv: 'production',
+    minimize: true
+  }
+};
