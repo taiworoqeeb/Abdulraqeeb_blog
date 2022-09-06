5939771859adcaa9e89a281531a62822354816b1
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-09-06 20:40:05 +0100

    -a

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,31 +37,31 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.700'),
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.600')} !important`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            h1: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
-            },
-            h2: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
-            },
-            h3: {
-              fontWeight: '600',
-              color: theme('colors.gray.900'),
-            },
-            'h4,h5,h6': {
-              color: theme('colors.gray.900'),
-            },
+            // color: theme('colors.gray.700'),
+            // a: {
+            //   color: theme('colors.primary.500'),
+            //   '&:hover': {
+            //     color: `${theme('colors.primary.600')} !important`,
+            //   },
+            //   code: { color: theme('colors.primary.400') },
+            // },
+            // h1: {
+            //   fontWeight: '700',
+            //   letterSpacing: theme('letterSpacing.tight'),
+            //   color: theme('colors.gray.900'),
+            // },
+            // h2: {
+            //   fontWeight: '700',
+            //   letterSpacing: theme('letterSpacing.tight'),
+            //   color: theme('colors.gray.900'),
+            // },
+            // h3: {
+            //   fontWeight: '600',
+            //   color: theme('colors.gray.900'),
+            // },
+            // 'h4,h5,h6': {
+            //   color: theme('colors.gray.900'),
+            // },
             pre: {
               backgroundColor: theme('colors.gray.800'),
             },
@@ -88,14 +88,14 @@ module.exports = {
               paddingBottom: '2px',
               borderRadius: '0.25rem',
             },
-            hr: { borderColor: theme('colors.gray.200') },
-            'ol li::marker': {
-              fontWeight: '600',
-              color: theme('colors.gray.500'),
-            },
-            'ul li::marker': {
-              backgroundColor: theme('colors.gray.500'),
-            },
+            // hr: { borderColor: theme('colors.gray.200') },
+            // 'ol li::marker': {
+            //   fontWeight: '600',
+            //   color: theme('colors.gray.500'),
+            // },
+            // 'ul li::marker': {
+            //   backgroundColor: theme('colors.gray.500'),
+            // },
             strong: { color: theme('colors.gray.600') },
             blockquote: {
               color: theme('colors.gray.900'),
@@ -105,31 +105,31 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme('colors.gray.300'),
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.400')} !important`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            h1: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
-            },
-            h2: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
-            },
-            h3: {
-              fontWeight: '600',
-              color: theme('colors.gray.100'),
-            },
-            'h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
+            // color: theme('colors.gray.300'),
+            // a: {
+            //   color: theme('colors.primary.500'),
+            //   '&:hover': {
+            //     color: `${theme('colors.primary.400')} !important`,
+            //   },
+            //   code: { color: theme('colors.primary.400') },
+            // },
+            // h1: {
+            //   fontWeight: '700',
+            //   letterSpacing: theme('letterSpacing.tight'),
+            //   color: theme('colors.gray.100'),
+            // },
+            // h2: {
+            //   fontWeight: '700',
+            //   letterSpacing: theme('letterSpacing.tight'),
+            //   color: theme('colors.gray.100'),
+            // },
+            // h3: {
+            //   fontWeight: '600',
+            //   color: theme('colors.gray.100'),
+            // },
+            // 'h4,h5,h6': {
+            //   color: theme('colors.gray.100'),
+            // },
             pre: {
               backgroundColor: theme('colors.gray.800'),
             },
@@ -139,14 +139,14 @@ module.exports = {
             details: {
               backgroundColor: theme('colors.gray.800'),
             },
-            hr: { borderColor: theme('colors.gray.700') },
-            'ol li::marker': {
-              fontWeight: '600',
-              color: theme('colors.gray.400'),
-            },
-            'ul li::marker': {
-              backgroundColor: theme('colors.gray.400'),
-            },
+            // hr: { borderColor: theme('colors.gray.700') },
+            // 'ol li::marker': {
+            //   fontWeight: '600',
+            //   color: theme('colors.gray.400'),
+            // },
+            // 'ul li::marker': {
+            //   backgroundColor: theme('colors.gray.400'),
+            // },
             strong: { color: theme('colors.gray.100') },
             thead: {
               th: {
