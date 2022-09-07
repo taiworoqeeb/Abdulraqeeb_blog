9532b4bdb5c872e4ae4b570bcc4367e5031c05fd
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-09-07 13:01:35 +0100

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,26 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[hash].css" })
   ],
-  optimization: {
+  configureWebpack:{
+    optimization: {
     nodeEnv: 'production',
     minimize: true,
-    runtimeChunk: 'single'
+    runtimeChunk: 'single',
+    splitChunks:{
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    }
   }
+  }
+  
 };
