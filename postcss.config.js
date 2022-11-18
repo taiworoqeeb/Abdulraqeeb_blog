87498d3c5ee51551d227194c65137229e7a2f402
taiworoqeeb <taiworoqeeb@gmail.com>
2022-11-18 20:20:54 +0100

    -a

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-const cssnano = require('cssnano')
 module.exports = {
   plugins: {
     tailwindcss: {config: './tailwind.config.js'},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? {cssnano: cssnano({preset: 'default'})} : {}) 
+    ...(process.env.NODE_ENV === 'production' ? {cssnano: {}} : {}) 
   }
 }
